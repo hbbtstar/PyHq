4ea612f5972a22bee5373f5d92697f5e9f76e520
alourenco <alexander.lourenco@gmail.com>
2014-03-26 13:15:25 +0100

    started skillplanner html + AJAX stuff

--- PyHq/apps/skillplanner/urls.py
+++ PyHq/apps/skillplanner/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from PyHq.apps.skillplanner import views
 
 urlpatterns = patterns('',
+    url(r'^api/get_skills/', views.get_skills, name='get_skills'),
     url(r'^$', views.skillplanner, name='skillplanner'),
 )
--- PyHq/apps/skillplanner/views.py
+++ PyHq/apps/skillplanner/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from PyHq.apps.characterscreen.models import Skill, SkillGroup, RequiredSkill
+import json
+from django.http import HttpResponse
 
 # Create your views here.
 def get_skills(request):
     if request.is_ajax():
         q = request.GET.get('term', '')
-        skills = []
-        return
+        skills = Skill.objects.filter(name__icontains=q)
+        print skills
+        results = []
+        for skill in skills:
+            skill_json = {}
+            skill_json['id'] = skill.skill_id
+            skill_json['label'] = skill.name
+            skill_json['value'] = skill.name
+            results.append(skill_json)
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype='application/json'
+    return HttpResponse(data, mimetype)
 
 def skillplanner(request):
     return render(request, 'skillplanner.html')
