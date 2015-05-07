02828e4988dc7b0ee9641a91a133e2ce3b0dc9da
alourenco <alexander.lourenco@gmail.com>
2015-05-07 10:15:47 +0200

    finished refactoring characterscreen stuff

--- PyHq/apps/characterscreen/tests.py
+++ PyHq/apps/characterscreen/tests.py
@@ -25,22 +25,12 @@ class CharacterScreenTestCase(TestCase):
         fake_skill = Skill(skill_id=99999, name='Fake Skill', group_id=2, description='test description', rank=3,
                            primaryAttribute='Intelligence', secondaryAttribute='Charisma')
         fake_skill.save()
-        fake_points = CharacterSkillPoints(skill=fake_skill, char=test_char, points=12345, level=3)
-        fake_points.save()
 
     def test_skill_update(self):
         #test that the skills are imported and generated correctly
         EveSkillsToDB()
         test_skill = Skill.objects.get(skill_id=3424)
         self.assertIn(test_skill.name, "Energy Grid Upgrades")
-
-    def test_skills_appear(self):
-        #test that skills appear in dropdown
-        c = Client()
-        c.login(username='test_user', password='test_pass')
-        response = c.get('/characterscreen/')
-        self.AssertIn(b'Fake Skill', response.content)
-
 
     #def test_char_stats_appear_to_logged_in_user(self):
         # test that the character's stats appear to the logged in user
