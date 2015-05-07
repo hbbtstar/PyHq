85bb16ca06a13fb253814ac89507367712fd7108
alourenco <alexander.lourenco@gmail.com>
2015-05-07 10:26:20 +0200

    finished refactoring characterscreen stuff, all tests pass in characterscreen

--- PyHq/apps/characterscreen/tests.py
+++ PyHq/apps/characterscreen/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from PyHq.apps.characterscreen.models import *
 from PyHq.libs.pyhqfunctions import *
 import evelink
+from PyHq.secret_settings import V_CODE, KEY_ID
 
 # Create your tests here.
 
@@ -17,7 +18,7 @@ class CharacterScreenTestCase(TestCase):
         print("Do nothing... yet")
         test_user = User.objects.create_user(username='test_user', email='f@f.com', password='test_pass')
         test_user.save()
-        test_account = Account(v_code='12345', key_id='12345', user=test_user)
+        test_account = Account(v_code=V_CODE, key_id=KEY_ID, user=test_user)
         test_account.save()
         test_char = Character(id=99999, name="Test Char", race="Test Race", bloodline="test_line", skill_points=1234567,
                               balance=12345678, account_id=test_account)
@@ -31,6 +32,15 @@ class CharacterScreenTestCase(TestCase):
         EveSkillsToDB()
         test_skill = Skill.objects.get(skill_id=3424)
         self.assertIn(test_skill.name, "Energy Grid Upgrades")
+
+    def test_skills_appear(self):
+        #test that skills appear in dropdown
+        c = Client()
+        EveSkillsToDB()
+        c.login(username='test_user', password='test_pass')
+        response = c.get('/characterscreen/')
+        self.assertIn(b'Spaceship Command', response.content)
+
 
     #def test_char_stats_appear_to_logged_in_user(self):
         # test that the character's stats appear to the logged in user
